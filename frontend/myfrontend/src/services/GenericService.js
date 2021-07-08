1bbf09870500c2357d573fcb1b41da12a13adcbb
Fariha31 <62641238+Fariha31@users.noreply.github.com>
2021-07-09 00:09:38 +0500

    ..

--- frontend/myfrontend/src/services/GenericService.js
+++ frontend/myfrontend/src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.baseURL = "/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
     get = (url) => {
